2af2bc4ab3dc0181d88ae3149391f87f420cd940
user <020200295a@uandina.edu.pe>
2024-03-31 22:46:21 -0700

    aoe

--- vite.config.js
+++ vite.config.js
@@ -14,24 +14,6 @@ export default defineConfig({
         main: resolve(__dirname,'index.html'),
         pacha8:resolve(__dirname,'pacha8/index.html')
       },
-      output:{
-chunkFileNames: 'assets/js/[name]-[hash].js',
-        entryFileNames: 'assets/js/[name]-[hash].js',
-        
-        assetFileNames: ({name}) => {
-          if (/\.(gif|jpe?g|png|svg)$/.test(name ?? '')){
-              return 'assets/images/[name]-[hash][extname]';
-          }
-          
-          if (/\.css$/.test(name ?? '')) {
-              return 'assets/css/[name]-[hash][extname]';   
-          }
- 
-          // default value
-          // ref: https://rollupjs.org/guide/en/#outputassetfilenames
-          return 'assets/[name]-[hash][extname]';
-        },
-      }
     }
   }
 });
